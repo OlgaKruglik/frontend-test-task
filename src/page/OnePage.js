f14b4bafb337425e6afa4dd4132aaceac8b34648
OlgaKruglik <137288699+OlgaKruglik@users.noreply.github.com>
2024-09-13 12:54:53 +0300

    page and sryles

---
+++ src/page/OnePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../style/OnePage.module.css'
+
+function OnePage() {
+    return (
+        <div className={styles.heading}>
+            <h1 className={styles.pageh1}>Book Store</h1>
+        </div>
+    )
+}
+
+export default OnePage
